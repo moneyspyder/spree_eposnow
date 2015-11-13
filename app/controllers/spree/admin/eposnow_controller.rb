beee425b54023fc106b5d5970824e361b59a0452
Warren Sangster <warren@moneyspyder.co.uk>
2015-11-13 15:54:55 +0000

    HH-70

--- app/controllers/spree/admin/eposnow_controller.rb
+++ app/controllers/spree/admin/eposnow_controller.rb
@@ -1,9 +1,75 @@
 class Spree::Admin::EposnowController < Spree::Admin::BaseController
 
-  def new
+  def index
   end
 
-  def index
+  # https://api.eposnowhq.com/api/V2/Product/{Product_ID}
+  def product
+    @product = {
+      'ProductID' => 6,
+      'Name' => "Tea",
+      'Description' => nil,
+      'CostPrice' => 0.3,
+      'SalePrice' => 1.5,
+      'EatOutPrice' => 0,
+      'CategoryID' => nil,
+      'Barcode' => nil,
+      'TaxRateID' => 33
+    }
+  end
+
+  # http://developer.eposnowhq.com/Docs/Product
+  # GET https://api.eposnowhq.com/api/V2/Product?page={Page_No}
+  def products
+    @products = [
+      {
+        'ProductID' => 6,
+        'Name' => "Tea",
+        'Description' => nil,
+        'CostPrice' => 0.3,
+        'SalePrice' => 1.5       
+      },
+      {
+        'ProductID' => 7,
+        'Name' => "Coffee",
+        'Description' => nil,
+        'CostPrice' => 0.3,
+        'SalePrice' => 1.6       
+      }             
+      # {
+      #   ProductID: 6,
+      #   Name: "Tea",
+      #   Description: nil,
+      #   CostPrice: 0.3,
+      #   SalePrice: 1.5,
+      #   EatOutPrice: 0,
+      #   CategoryID: nil,
+      #   Barcode: nil,
+      #   TaxRateID: 33,
+      #   EatOutTaxRateID: nil,
+      #   BrandID: nil,
+      #   SupplierID: nil,
+      #   PopupNoteID: nil,
+      #   UnitOfSale: nil,
+      #   VolumeOfSale: nil,
+      #   MultiChoiceID: nil,
+      #   ColourID: nil,
+      #   VariantGroupID: nil,
+      #   Size: nil,
+      #   Sku: nil,
+      #   SellOnWeb: false,
+      #   SellOnTill: true,
+      #   OrderCode: nil,
+      #   ButtonColourID: nil,
+      #   SortPosition: nil,
+      #   MagentoAttributeSetID: nil,
+      #   RRPrice: nil,
+      #   CostPriceTaxRateID: nil,
+      #   ProductType: 0,
+      #   TareWeight: nil,
+      #   ArticleCode: nil        
+      # }
+    ]
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Spree::Core::Engine.routes.draw do
 
   namespace :admin do
-    get :eposnow, to: 'eposnow#index'
+    #get :eposnow, to: 'eposnow#index'
+    resources :eposnow, only: [:index] do
+      member do
+        get :product
+      end
+      collection do
+        get :products
+      end
+    end
   end
 
 end
