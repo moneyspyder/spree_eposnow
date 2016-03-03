ee3bfa18fc83fd4b977e2e06f862fb6794d83e84
Warren Sangster <warren@moneyspyder.co.uk>
2016-03-03 22:39:42 +0000

    HH-175

--- spec/models/spree/eposnow/product_spec.rb
+++ spec/models/spree/eposnow/product_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Spree::Eposnow::Product do
   before(:each) do
     Spree::Config[:eposnow_key] = '2ND36E1H9HWAMCAR2P8QCJFGPUQVOLF0'
     Spree::Config[:eposnow_secret] = '868MZR5KHSNQYGVYSYK8N8QN1EV402DG'
+    Spree::Eposnow::Product.basic_auth(Spree::Config[:eposnow_key],Spree::Config[:eposnow_secret])
   end    
   
   describe '#sync' do
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,6 +34,7 @@ require 'spree/testing_support/factories'
 require 'spree/testing_support/url_helpers'
 require 'spree/testing_support/controller_requests'
 require 'spree/testing_support/authorization_helpers'
+require 'spree/testing_support/preferences'
 
 FactoryGirl.find_definitions
 
